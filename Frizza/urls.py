95300d89c3114ccc824ff034d2c98d890ad9c141
Tyler Allen <xtallenz@gmail.com>
2014-04-17 19:06:24 -0400

    changes to try to implement login

--- Frizza/urls.py
+++ Frizza/urls.py
@@ -10,8 +10,8 @@ from django.template import loader
 
 urlpatterns = patterns('',
     # View for login page.
-    (r'^accounts/login/$',  login, loader.get_template(settings.TEMPLATE_DIRS + '/public_html/login')),
-    (r'^accounts/logout/$', logout, loader.get_template(settings.TEMPLATE_DIRS + '/public_html/Goodbye/goodbye')),
+    (r'^accounts/login/$',  login, loader.get_template(settings.TEMPLATE_DIRS + '/public_html/login.html')),
+    (r'^accounts/logout/$', logout, loader.get_template(settings.TEMPLATE_DIRS + '/public_html/Goodbye/goodbye.html')),
     #url(r'^login/', 'Frizza.views.login'), #views.LoginView.as_view()),
     # Login is the default page.
     url(r'^$', 'Frizza.views.login', name='login'),
