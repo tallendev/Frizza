21d9c9aeec62ad2c362bb71f10abecf46df17d76
tyler <tyler@tyler-db.(none)>
2014-04-19 23:49:38 -0400

    added database filler script

---
+++ pizza_build.py
@@ -0,0 +1,17 @@
+
+from Frizza.models import Sauce
+Sauce('Tomato', 100).save()
+Sauce('Alfredo', 100).save()
+Sauce('Olive Oil', 100).save()
+from Frizza.models import Crust
+Crust('Thick', 100).save()
+Crust('Thin', 100).save()
+Crust('Classic', 100).save()
+from Frizza.models import Topping
+Topping('Pepperoni', 100).save()
+Topping('Olives', 100).save()
+from Frizza.models import Pizza
+Pizza(1, 'Pepperoni', 1, 'Alfredo', 'Thin').save()
+Pizza(2, 'Cheese', 1, 'Alfredo', 'Thin').save()
+from Frizza.models import HasTopping
+
