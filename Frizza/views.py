631018a8dc464514deec7265829de2bc47037254
Tyler Allen <xtallenz@gmail.com>
2014-04-21 08:40:37 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Frizza/templates/public_html/login.html

--- Frizza/views.py
+++ Frizza/views.py
@@ -16,10 +16,10 @@ logger = logging.getLogger('registration')
 # page.
 def pizza(request):
     if request.user.is_authenticated():
-        order_list = Orders.objects.filter(user_name="admin")
+        order_list = Orders.objects.filter(user_name__in="admin")
         admin_list = Pizza.objects.filter(pizza_id=order_list).select_related()
         uorder_list = Orders.objects.filter(user_name=str(request.user))
-        user_list = Pizza.objects.filter(pizza_id=uorder_list).select_related()
+        user_list = Pizza.objects.filter(pizza_id__in=uorder_list).select_related()
         context = {'admin_list': admin_list,
                    'user_list': user_list}
         #TODO add user stufft
