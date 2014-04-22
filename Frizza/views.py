96817f816afe82a9a72468853db2aebad947d921
Hayden <hayden@ubuntu.(none)>
2014-04-22 14:24:50 -0400

    Updated waste and return pages.

--- Frizza/views.py
+++ Frizza/views.py
@@ -298,14 +298,14 @@ def waste(request):
 
     if request.user.is_authenticated():
         if 'return_pizza' in request.session:
-            pizza = Pizza.objects.get(pizza_name=str(request.return_pizza))
+            pizza = Pizza.objects.get(pizza_name=str(request.session['return_pizza']))
             wasted_toppings = HasTopping.objects.filter(pizza_id=pizza.pizza_id).\
                                          select_related('orders__pizza_name')
 
-            wasted_sauce = Pizza.objects.filter(pizza_name=str(request.return_pizza)).\
+            wasted_sauce = Pizza.objects.filter(pizza_name=str(request.session['return_pizza'])).\
                                  select_related('orders__pizza_name')
 
-            wasted_crust = Pizza.objects.filter(pizza_name=str(request.return_pizza)).\
+            wasted_crust = Pizza.objects.filter(pizza_name=str(request.['return_pizza'])).\
                                  select_related('orders__pizza_name')
 
             context = {'wasted_toppings': wasted_toppings,
