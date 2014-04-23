fa60b3335b39dbc30edd658b22f169734f0d451d
Tyler Allen <xtallenz@gmail.com>
2014-04-23 06:13:31 -0400

    whee

--- Frizza/views.py
+++ Frizza/views.py
@@ -1,3 +1,21 @@
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Tyler Allen, Trevor Griggs, Hayden Thomas
+"""
+
+
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect
@@ -6,7 +24,6 @@ from django.db.models import Max
 import settings
 from Frizza.models import User, Sauce, Crust, Pizza, Topping, HasTopping, \
     Allergy, Orders
-
 
 """ This function provides an appropriate response to a request for the pizza
     page. It will generate the available pizza selections, as well as
