f8a830e1d52b3f0ad1c3d82d331f478664ffdf8c
Tyler Allen <xtallenz@gmail.com>
2014-04-23 01:07:54 -0400

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	Frizza/templates/public_html/login.html

--- Frizza/views.py
+++ Frizza/views.py
@@ -111,7 +111,7 @@ def sauce(request):
                         return HttpResponseRedirect('/toppings')
                     else:
                         request.session['sauce_error'] = True
-                        HttpResponseRedirect('/toppings')
+                        HttpResponseRedirect('/sauce')
                 else:
                     del request.session['crust']
                     return HttpResponseRedirect('/crust')
